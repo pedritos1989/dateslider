6680f59b2bf54c19fca980e4474ec0b2e2139c07
Yainel García Alfonso <garcia.yainel@gmail.com>
2020-08-20 08:53:50 -0400

    Primer commit

---
+++ js/sly-horizontal.js
@@ -0,0 +1,32 @@
+jQuery(function($) {
+  'use strict';
+
+  // -------------------------------------------------------------
+  //   Centered Navigation
+  // -------------------------------------------------------------
+  (function() {
+    var $frame = $('#centered');
+    var $wrap = $frame.parent();
+
+    // Call Sly on frame
+    $frame.sly({
+      horizontal: 1,
+      itemNav: 'centered',
+      smart: 1,
+      activateOn: 'click',
+      mouseDragging: 1,
+      touchDragging: 1,
+      releaseSwing: 1,
+      startAt: 8,
+      scrollBar: $wrap.find('.scrollbar'),
+      scrollBy: 1,
+      speed: 300,
+      elasticBounds: 1,
+      easing: 'easeOutExpo',
+      dragHandle: 1,
+      dynamicHandle: 1,
+      clickBar: 1,
+    });
+  }());
+});
+
